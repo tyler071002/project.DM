92338cc85e9b0fc09e141558ad68ab0a8ec0dbad
sims_0 <112733687+tyler071002@users.noreply.github.com>
2024-10-28 18:46:17 +0900

    학교노트북 환경설정파일

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,2 @@
-import os
-from PyKakao import KoGPT
-
-# 서비스 키를 환경 변수에서 가져오기
-service_key = os.environ.get('KAKAO_API_KEY')
-
-# KoGPT API 인스턴스 생성
-GPT = KoGPT(service_key=service_key)
-
-# 다음 문장 만들기
-prompt = "인간처럼 생각하고, 행동하는 '지능'을 통해 인류가 이제까지 풀지 못했던"
-max_tokens = 64
-
-try:
-    result = GPT.generate(prompt, max_tokens, temperature=0.7, top_p=0.8)
-    print(result)
-except Exception as e:
-    print("오류가 발생했습니다:", e)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
