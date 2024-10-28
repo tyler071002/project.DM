f8780c6297fca30a83ffbada7b6c4c195dcca651
kibeom07 <gim941124@gmail.com>
2024-10-28 18:45:12 +0900

    html
    update

--- Web/script.js
+++ Web/script.js
@@ -1,5 +1,6 @@
 const messageInput = document.getElementById("message-input");
 const sendButton = document.getElementById("send-button");
+const main = document.getElementById("wrap");
 
 messageInput.addEventListener('input', () => {
     messageInput.style.height = 'auto';
@@ -7,10 +8,18 @@ messageInput.addEventListener('input', () => {
 });
 
 sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message) {
-        console.log("전송된 메시지:", message); // 여기에서 실제 전송 기능 구현
-        messageInput.value = ''; // 메시지 입력창 초기화
+    const user_message = messageInput.value;
+    if (user_message) {
+        const messageElement = document.createElement('div');
+        messageElement.textContent = user_message;
+        messageElement.classList.add('user-message-div');
+
+        main.appendChild(messageElement);
+
+        console.log("전송된 메시지:", user_message); // 실제 전송 기능 구현
+
+        // 메시지 입력창 초기화
+        messageInput.value = '';
         messageInput.style.height = 'auto'; // 높이 초기화
     }
 });
